a58c36bdda711988ed81dfce801dadb7287faa84
hamzasaud <hamzasaud95@gmail.com>
2025-10-29 09:22:31 +0500

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css';
 import Layout from '../components/Layout';
 import { LanguageProvider } from '../lib/LanguageContext';
+import { Analytics } from '@vercel/analytics/next';
 
 export default function App({ Component, pageProps }) {
   // Check if the page has a custom layout
@@ -9,6 +10,7 @@ export default function App({ Component, pageProps }) {
   return (
     <LanguageProvider>
       {getLayout(<Component {...pageProps} />)}
+      <Analytics />
     </LanguageProvider>
   );
 }
